c63fb58fd205bbfb7a33ad86581012d86b71727c
lincal2309 <christophe.gressin@tofographies.com>
2018-10-08 19:21:44 +0200

    Correction du répertoire des ressources pour portabilité

--- Donnees.py
+++ Donnees.py
@@ -12,9 +12,10 @@ from observers import *
 class Donnees:
     def __init__(self):
         # Labyrinth's and window's size
-        self.lab_dim = 15
+        self.lab_dim = 15 # Number of cels in each direction
+
         self.step = 60 # A REVOIR - variable de pas pour gérer le déplacement - dépend de la taille de simages et de la grille
-        self.screen_size = self.screen_width, self.screen_height = self.step * self.lab_dim, self.step * (self.lab_dim + 1)
+        self.display_size = self.display_width, self.display_height = self.step * self.lab_dim, self.step * (self.lab_dim + 1)
 
         # Name of the file that defines the grid
         self.grid_file = "grille.txt"
@@ -23,7 +24,7 @@ class Donnees:
         self.object_list = ["Seringue", "Aiguille", "Ether"]
 
         # Pictures folder
-        self.ressources_path = os.path.dirname(".\\Ressources\\")
+        self.ressources_path = "Ressources"
 
         # List of pictures
         self.MacGyver_file = "MacGyver.png"
